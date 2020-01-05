66e54be0c48cc6db134737d2ddb716677707d6c5
Marcus Francis <marcusmagnusson@outlook.com>
2020-01-05 22:42:32 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,31 @@
-import { NextPage } from 'next'
-import Hello from 'components/Hello'
+import { NextPage, NextPageContext, NextComponentType } from 'next';
+import Hello from 'components/Hello';
+import gql from 'graphql-tag';
 
-const Home: NextPage = () => <Hello />;
+interface Context extends NextPageContext {
+	apolloClient: any;
+}
+
+interface Props {
+	data: any;
+}
+
+const Home: NextPage<Props> = ({ data }) => {
+	return <Hello name={data.name} />
+};
+
+Home.getInitialProps = async ({ apolloClient }: Context) => {
+	const { loading, errors, data } = await apolloClient.query({ query: getMe });
+	return { data: data.me };
+};
+
+const getMe = gql`
+	query {
+		me {
+			name
+			id
+		}
+	}
+`;
+
 export default Home;

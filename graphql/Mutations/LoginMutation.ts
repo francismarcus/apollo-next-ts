0ed7718a94010667ccfeae494ebced309047c6ed
Marcus Francis <marcusmagnusson@outlook.com>
2020-01-06 04:52:15 +0100

    Create LoginMutation.ts

---
+++ graphql/Mutations/LoginMutation.ts
@@ -0,0 +1,31 @@
+import { useMutation, useApolloClient } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const loginMutation = gql`
+	mutation LoginMutation($credentials: LoginInput!) {
+		login(credentials: $credentials) {
+			token
+			user {
+				id
+			}
+		}
+	}
+`;
+
+export default () => {
+	const client = useApolloClient();
+	const [mutate, { data }] = useMutation(loginMutation);
+
+	return async (email, password) => {
+		const response = await mutate({
+			variables: { credentials: { email, password } }
+		});
+
+		const { token } = response.data.login;
+		return client.writeData({
+			data: {
+				myToken: token
+			}
+		});
+	};
+};

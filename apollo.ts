9231019ede46541fd228abbf873f4e24b47b3dac
Marcus Francis <marcusmagnusson@outlook.com>
2020-01-06 04:51:50 +0100

    Update apollo.ts

--- apollo.ts
+++ apollo.ts
@@ -1,50 +1,39 @@
-
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink, createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
-import gql from 'graphql-tag';
 import withApollo from 'next-with-apollo';
-
-const getMyToken = gql`
-	{
-		myToken @client
-	}
-`;
+import { getMyToken } from 'graphql/queries'
 
 const httpLink = createHttpLink({
 	uri: 'http://localhost:4000/graphql'
 });
 
-
 const data = {
-    myToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZTA3YzA5YmFmZTg3NjM4NTBhZDdlNjgiLCJpYXQiOjE1Nzc3NDI3MTd9.Hd1Y64VebxAu45dxt73vBuPEc1N1RR2lglVVPTYIs-k"
-}
+	myToken:
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZTA3YzA5YmFmZTg3NjM4NTBhZDdlNjgiLCJpYXQiOjE1Nzc3NDI3MTd9.Hd1Y64VebxAu45dxt73vBuPEc1N1RR2lglVVPTYIs-k'
+};
 
-export default withApollo(
-    ({ initialState }) => {
-        const cache = new InMemoryCache().restore(initialState || {})
-        cache.writeData({
-            data
-        })
-        const authLink = setContext(async (_, { headers }) => {
-            const store: any = await cache.readQuery({
-                query: getMyToken
-            });
-            const { myToken } = store
-            return {
-                headers: {
-                    ...headers,
-                    authorization: myToken ? `${myToken}` : ""
-                }
-            };
-        })
-        const link = authLink.concat(httpLink);
-        return new ApolloClient({
-            cache,
-			link
-		})
-    }
-    
-		
-);
+export default withApollo(({ initialState }) => {
+	const cache = new InMemoryCache().restore(initialState || {});
+	cache.writeData({
+		data
+	});
+	const authLink = setContext(async (_, { headers }) => {
+		const store: any = await cache.readQuery({
+			query: getMyToken
+		});
+		const { myToken } = store;
+		return {
+			headers: {
+				...headers,
+				authorization: myToken ? `${myToken}` : ''
+			}
+		};
+	});
+	const link = authLink.concat(httpLink);
+	return new ApolloClient({
+		cache,
+		link
+	});
+});

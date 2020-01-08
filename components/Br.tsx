bb2e37a57459604ddd7046292835bc9450e430e8
Marcus Francis <marcusmagnusson@outlook.com>
2020-01-08 01:12:46 +0100

    Create Br.tsx

---
+++ components/Br.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Br = styled.div`
+    width: 100%;
+    height: 1px;
+    margin: 20px 0 20px 0;
+    background-color: #e3e3e3;
+`
+
+export default Br

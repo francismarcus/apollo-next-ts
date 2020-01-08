a803d12c0aed48851e3b9f789170a7bae22e2568
Marcus Francis <marcusmagnusson@outlook.com>
2020-01-08 01:13:45 +0100

    Create meQuery.tsx

---
+++ graphql/Query/meQuery.tsx
@@ -0,0 +1,57 @@
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import * as React from 'react';
+
+export const meQuery = gql`
+	{
+		me {
+			id
+			name
+			programs {
+				name
+				id
+			}
+		}
+	}
+`;
+
+export interface Me {
+	id: string;
+	name: string;
+}
+
+interface WithMe {
+	me: any;
+	loading: boolean;
+}
+
+interface Props {
+	children: (data: WithMe) => JSX.Element;
+}
+
+interface Data {
+	me: Me;
+}
+
+interface Variables {}
+
+export default class MeQuery extends React.PureComponent<Props> {
+	public render() {
+		return (
+			<Query<Data> query={meQuery}>
+				{({ data, loading }) => {
+					let me = null;
+
+					if (data && data.me) {
+						me = data.me;
+					}
+
+					return this.props.children({
+						me,
+						loading
+					});
+				}}
+			</Query>
+		);
+	}
+}

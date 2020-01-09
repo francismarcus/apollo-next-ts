ed9fb9a53e9c45c312c8b7c04807ddbf551be90e
francismarcus <marcusmagnusson@outlook.com>
2020-01-09 10:34:23 +0100

    update Login C

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,11 +1,8 @@
-import { Form, Field, Formik } from 'formik';
-import FormInput from 'components/FormInput';
-import Button from 'components/Button';
-import Br from 'components/Br';
-import { Text } from './style';
+import { Formik } from 'formik';
+import LoginForm from './LoginForm'
 import { schema } from './schema';
 import { loginMutation, saveToken } from 'graphql/Mutations/LoginMutation'
-import { useMutation, useApolloClient } from '@apollo/react-hooks'
+import { useMutation } from '@apollo/react-hooks'
 import { NextComponentType } from 'next';
 
 const Login: React.FC = () => {
@@ -22,30 +19,18 @@ const Login: React.FC = () => {
 				const response = await mutate(({
 					variables: { credentials: { email, password } }
 				}))
-				
+
 				const { token } = response.data.login
-				if(token && client) {
-				saveToken(token, client)
-				console.log('push to dashboard')
+				if (token && client) {
+					saveToken(token, client)
+					console.log('push to dashboard')
 				}
 			}}
 		>
 			{({ submitForm }) => (
-				<Form>
-					<Field name="email" placeholder="Email" label="Email" component={FormInput} />
-					<Field
-						name="password"
-						placeholder="Password"
-						label="Password"
-						type="password"
-						component={FormInput}
-					/>
-
-					<Button onClick={submitForm}>Login</Button>
-					{error && <Text> {error.graphQLErrors[0].message} </Text>}
-					<Br />
-					<Text onClick={() => console.log('Route to register page')}>Register</Text>
-				</Form>
+				<LoginForm
+					submitForm={submitForm}
+					error={error} />
 			)}
 		</Formik>
 	);
@@ -57,3 +42,4 @@ interface FieldProps {
 }
 
 export default Login as NextComponentType
+

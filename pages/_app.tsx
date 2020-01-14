289c2a1a3cce20d8e65e179d1555ae9fa090f02b
Marcus Francis <marcusmagnusson@outlook.com>
2020-01-14 23:32:58 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,59 @@
-import React from 'react'
-import App from 'next/app'
-import { ApolloProvider } from '@apollo/react-hooks'
-import withApollo from '../apollo'
-import { createGlobalStyle, ThemeProvider } from "styled-components";
+import React from 'react';
+import App from 'next/app';
+import { ApolloProvider } from '@apollo/react-hooks';
+import withApollo from '../apollo';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Montserrat);
   body {
     font-family: 'Montserrat', sans-serif;
+    background-color: ${({ theme }) => theme.bgColor};
+	color: ${({ theme }) => theme.color};
+	height: 100vh;
+	width: 100%;
   }
-`
+`;
 
 interface Props {
-    apollo: any
+	apollo: any;
 }
 
-const theme = {
-    color: {
-        main: '#ff5a5f;',
-        secondary: '#5AFFFA;'
-    }
-}
+const themes = {
+	light: {
+		type: 'light',
+		bgColor: 'white',
+		color: '#ff5a5f'
+	},
+	dark: {
+		type: 'dark',
+		bgColor: '#1F2933',
+		color: '#5AFFFA'
+	}
+};
 
 class MyApp extends App<Props> {
-    render() {
-        const { Component, pageProps, apollo } = this.props
-        return (
-            <ApolloProvider client={apollo}>
-                <ThemeProvider theme={theme}>
-                    <GlobalStyles />
-                    <Component {...pageProps} />
-                </ThemeProvider>
-            </ApolloProvider>
-        )
-    }
+	state = {
+		theme: true
+	}
+
+	changeTheme = () => {
+		this.setState((state: any)=> ({
+			theme: !state.theme
+		}))
+	}
+	render() {
+		const { Component, pageProps, apollo } = this.props;
+		const { theme } = this.state
+		return (
+			<ApolloProvider client={apollo}>
+				<ThemeProvider theme={theme === true ? themes.light : themes.dark}>
+					<GlobalStyles />
+					<Component {...pageProps} changeTheme={this.changeTheme} />
+				</ThemeProvider>
+			</ApolloProvider>
+		);
+	}
 }
 
-export default withApollo(MyApp)
+export default withApollo(MyApp);

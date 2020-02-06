296dfa7b2920dc22187e043e90e91d9cdc24ca77
Marcus Francis <marcusmagnusson@outlook.com>
2020-02-06 02:07:42 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const withImages = require('next-images');
+const withCSS = require('@zeit/next-css');
 
-module.exports = {
-  target: "serverless",
-  webpack: (config, options) => {
-    if (config.resolve.plugins) {
-      config.resolve.plugins.push(new TsconfigPathsPlugin());
-    } else {
-      config.resolve.plugins = [new TsconfigPathsPlugin()];
-    }
-    return config;
-  }
-};
+module.exports = withCSS(
+	withImages({
+		target: 'serverless',
+		webpack: (config, options) => {
+			if (config.resolve.plugins) {
+				config.resolve.plugins.push(new TsconfigPathsPlugin());
+			} else {
+				config.resolve.plugins = [new TsconfigPathsPlugin()];
+			}
+			return config;
+		},
+		loaders: [
+			{
+				test: /\.css$/,
+				loader: 'style!css!'
+			}
+		]
+	})
+);

670091334775a33a1e8725efff5c8d8763a654e7
Marcus Francis <marcusmagnusson@outlook.com>
2020-02-14 22:08:27 +0100

    Input Error fix

--- components/FormInput/index.tsx
+++ components/FormInput/index.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import { Container, Label, Input, ErrorMessage } from './style';
-import { FieldProps, useField } from 'formik';
+import { FieldInputProps, useField } from 'formik';
 
-const FormInput = ({ label, ...props }: Props) => {
+const FormInput: React.FC<any> = ({ label, ...props }) => {
 	const [field, meta] = useField(props);
 	const errorMsg = meta.touched && meta.error;
 	return (
 		<Container>
 			{label && <Label htmlFor={props.id || props.name}> {label} </Label>}
-			<Input
-				{...field} {...props} 
-			/>
+			<Input {...field} {...props} error={!!errorMsg} />
 			{errorMsg && <ErrorMessage> {meta.error} </ErrorMessage>}
 		</Container>
 	);
 };
 
-type Props = any
+interface Props<T> extends FieldInputProps<T> {
+	label: string;
+	id?: string;
+}
+
 export default FormInput;
